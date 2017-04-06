61d32ace915b989fe547dee9fd20ea5b48e07221
William Nguyen <WDDnguyen@gmail.com>
2017-04-06 15:48:58 -0400

    added files for the split cache

---
+++ Pipelined Processor files/split_cache/PC.vhd
@@ -0,0 +1,33 @@
+library ieee;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity pc is
+port(clk : in std_logic;
+	 reset : in std_logic;
+	 counterOutput : out std_logic_vector(31 downto 0);
+	 counterInput : in std_logic_vector(31 downto 0) := x"00000000"
+	 );
+end pc;
+
+architecture pc_arch of pc is
+
+ 
+begin
+
+process (clk,reset)
+begin
+	
+	if (reset = '1') then
+		counterOutput <= x"00000000";
+	elsif (clk'event and clk = '1') then 	
+		counterOutput <= counterInput;
+
+	end if;
+	
+	
+	end process;
+
+
+	
+end pc_arch;
