53660dfff948abd813c471f09550c473873bd492
Joel Lat <jlat2830@gmail.com>
2017-04-11 14:18:30 -0400

    Updated testbenchs to better show improvements

--- Pipelined Processor files/Testbench/ECO_testbench.asm
+++ Pipelined Processor files/Testbench/ECO_testbench.asm
@@ -13,7 +13,7 @@ add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
 
-addi $10, $0, 6 	#R10 initialized to '6'
+addi $10, $0, 6 	#R10 initialized to '10'
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
@@ -21,64 +21,24 @@ addi $1, $0, 1 		#R1 initialized to '1'
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-addi $2, $0, 7		#R2 initialized to '7'
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $3, $0, 8		#R3 initialized to '8'
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $4, $0, 9		#R4 initialized to '9'
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
 j Loop
 
-Loop: add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $1, $1, 1 	#R1 Final Value: 6
+Loop: addi $1, $1, 1 	#R1 Final Value: 10
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-sw $2, 3($9)		#Value of R2 stored into given address
+sw $1, 4($0)		#Value of R2 stored into given address
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-sw $3, 4($8)		#Value of R3 stored into given address
+lw $2, 4($0)		#Value of R3 stored into given address
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-sw $4, 5($7)		#Value of R4 stored into given address
+beq $10, $2, End 	#Loop execution terminates after five iterations
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-lw $2, 3($9)		#Value of R2 loaded from address it just stored to
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $2, $2, 1		#Value of R2 incremented by 1
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-lw $3, 4($8)		#Value of R3 loaded from address it just stored to
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $3, $3, 1		#Value of R3 incremented by 1
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-lw $4, 5($7)		#Value of R4 loaded from address it just stored to
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $4, $4, 1		#Value of R4 incremented by 1
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-beq $10, $1, End 	#Loop execution terminates after five iterations
 j Loop
 
 End: add $0, $0, $0
--- Pipelined Processor files/Testbench/UCO_testbench.asm
+++ Pipelined Processor files/Testbench/UCO_testbench.asm
@@ -15,7 +15,7 @@ addi $1, $0, 1
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-addi $31, $0, 8
+addi $31, $0, 15
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
@@ -24,130 +24,27 @@ j Loop
 Loop: add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-addi $1, $1, 1  # R1 final value: 5
+addi $1, $1, 1  # R1 final value: 12
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-addi $2, $1, 1   # R2 final value: 6
+addi $2, $1, 1   # R2 final value: 13
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-addi $3, $2, 1   # R3 final value: 7
+addi $3, $2, 1   # R3 final value: 14
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-addi $4, $3, 1   # R4 final value: 8
+addi $4, $3, 1   # R4 final value: 15
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-addi $5, $4, 1   # R5 final value: 9
+addi $5, $4, 1   # R5 final value: 16
 add $0, $0, $0
 add $0, $0, $0
 add $0, $0, $0
-addi $6, $5, 1   # R6 final value: 10
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $7, $6, 1   # R7 final value: 11
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $8, $7, 1   # R8 final value: 12
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $9, $8, 1   # R9 final value: 13
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $10, $9, 1  # R10 final value: 14
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $11, $10, 1 # R11 final value: 15
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $12, $11, 1 # R12 final value: 16
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $13, $12, 1 # R13 final value: 17
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $14, $13, 1 # R14 final value: 18
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $15, $14, 1 # R15 final value: 19
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $16, $15, 1 # R16 final value: 20
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $17, $16, 1 # R17 final value: 21
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $18, $17, 1 # R18 final value: 22
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $19, $18, 1 # R19 final value: 23
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $20, $19, 1 # R20 final value: 24
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $21, $20, 1 # R21 final value: 25
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $22, $21, 1 # R22 final value: 26
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $23, $22, 1 # R23 final value: 27
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $24, $23, 1 # R24 final value: 28
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $25, $24, 1 # R25 final value: 29
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $26, $25, 1 # R26 final value: 30
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $27, $26, 1 # R27 final value: 31
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $28, $27, 1 # R28 final value: 32
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $29, $28, 1 # R29 final value: 33
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-addi $30, $29, 1 # R30 final value: 34
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
-beq $31, $4, End # R31 final value: 8
-add $0, $0, $0
-add $0, $0, $0
-add $0, $0, $0
+beq $31, $4, End # R31 final value: 15
 j Loop
 
 End: add $0, $0, $0
